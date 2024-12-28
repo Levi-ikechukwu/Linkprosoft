b02f1623a32654f5bc4bf8aa69a9685d2fc9e864
Levi Ikechukwu <leviikechukwu12@gmail.com>
2024-12-28 20:42:19 +0100

    TT

--- src/data/feedData.js
+++ src/data/feedData.js
@@ -1,7 +1,7 @@
 import img3 from '../assets/images/img1.png'
 import img1 from '../assets/images/img2.png'
 import img2 from '../assets/images/img3.png'
-import profile from '../assets/images/person6.png'
+import profile from '../assets/images/Person6.png'
 
 
 
